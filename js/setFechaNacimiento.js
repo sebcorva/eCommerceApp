f0f7f3bf0419231957cdf9a040c16a55c7049f9d
Sebastian Corvalan espinoza <se.corvalan@duocuc.cl>
2026-05-31 19:41:58 -0400

    correccion de responsividad movil

---
+++ js/setFechaNacimiento.js
@@ -0,0 +1,10 @@
+flatpickr("#fecha_nacimiento", {
+    locale: "es",          
+    dateFormat: "d-m-Y",   
+    maxDate: "today",     
+    disableMobile: "true", 
+
+    changeMonth: true,   
+    changeYear: true, 
+    yearRange: "1920:2026" 
+});
